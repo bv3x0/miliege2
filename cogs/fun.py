3707ca26c7910e9f9627a8c03ae0a2a9915a3457
Your Name <dscoop@gmail.com>
2025-02-22 14:43:24 -0500

    Added fun commands

--- cogs/fun.py
+++ cogs/fun.py
@@ -35,6 +35,7 @@ class FunCommands(commands.Cog):
             "https://i.poastcdn.org/bc/58/6f/bc586ffb8d29efd0ed8d4dbf0cf450fddd67f9e9e9090f3d0bc88fe3c3f70420.png",
             "https://media.discordapp.net/attachments/1149697700418306158/1339987703214637056/Gjwk9HbW8AAGiYt.png?ex=67b9f2cc&is=67b8a14c&hm=7af9b747846292f387806c510fe1fb39d083ce2a0ad42bbf5912b77cdc46856b&=&format=webp&quality=lossless",
             "https://pbs.twimg.com/media/Gdx0pZxaEAA7b4L?format=jpg&name=large",
+            "https://pbs.twimg.com/media/F5At7etXMAEGZoj?format=jpg&name=medium",
             "https://pbs.twimg.com/media/GkRudWxWQAAeCL4?format=jpg&name=small",
             "https://pbs.twimg.com/media/GZzA4QiWsAAX25d?format=jpg&name=small",
             "https://pbs.twimg.com/media/Ga5XIPKXEAAJ_p9?format=jpg&name=900x900",
@@ -43,6 +44,9 @@ class FunCommands(commands.Cog):
             "https://cdn.discordapp.com/attachments/1149879412100190248/1325873532948709518/hope_2.mp4?ex=67ba0473&is=67b8b2f3&hm=4a47ea3d374521192b4bec48a0afbeaef21ca8ba4fb227980248d285e10a3f4a&",
             "https://cdn.discordapp.com/attachments/1149697700418306158/1325899391000641536/hope_2.5.mp4?ex=67b973c8&is=67b82248&hm=dcfb2a92318664fc0021fe0ed37971a4eb511db67521ac764ed585e48b8cf8c9&",
             "https://media.discordapp.net/attachments/1149697700418306158/1342223139156791297/image0.jpg?ex=67b982f5&is=67b83175&hm=4aa4e4bf7736adbbf25456bf948bab7f284b8f7e390e40761837c3bb8d19d9c8&=&format=webp&width=662&height=919",
+            "https://media.discordapp.net/attachments/1180965924459778151/1342936149693304973/IMG_0247.jpg?ex=67bb7240&is=67ba20c0&hm=f586af351ac6bc2719e6795a46a2491e4b148da83f508b3a16edbcd279ac9fcd&=&format=webp&width=607&height=920",
+            "https://cdn.discordapp.com/attachments/1180965924459778151/1342944112856268940/Screenshot_2025-02-22_at_2.39.49_PM.png?ex=67bb79ab&is=67ba282b&hm=cbd6601fa219e014bf8360aacd503f960618f05650877a46bf80670a183dcfea&",
+            "https://media.discordapp.net/attachments/1180965924459778151/1342944127712759829/Screenshot_2025-02-22_at_2.39.39_PM.png?ex=67bb79ae&is=67ba282e&hm=fb52e7bd1030ecfde77ef4d8c258140a5c2ffb596c05c079b6e50b3909a6df27&=&format=webp&quality=lossless",
         ]
 
     @commands.command()
